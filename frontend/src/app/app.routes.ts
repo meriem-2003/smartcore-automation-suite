fb22d02ee80083275087f161fdaa4e6b55e3e28d
Meriem Khoubbaz <khoubbazmeriem@gmail.com>
2026-06-30 23:53:12 +0100

    feat: Angular standalone scaffolding, Login et Dashboard (CDC Semaine 1)

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,19 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./features/auth/login/login').then(m => m.Login)
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./features/dashboard/dashboard').then(m => m.Dashboard)
+  }
+];
